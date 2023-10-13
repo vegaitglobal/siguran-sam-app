32702363ad6a18ae5e1ed0054517c71da62be0b3
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2023-10-13 18:44:26 +0200

    Add react hooks config

--- siguran-sam-expo/.eslintrc.js
+++ siguran-sam-expo/.eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: ["@react-native-community", "plugin:prettier/recommended"],
-  plugins: ["@typescript-eslint", "prettier"],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native-community',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     // Add any custom rules or overrides here
   },
