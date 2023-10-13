b0b5dd3a264bb981277641d6ae0edb7f4c2f53f4
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2023-10-13 20:08:05 +0200

    Add path alias

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ExampleComponent from "@/domain/example-feature/components/example-component";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ExampleComponent exampleProp={1} />
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
