776b7346f46730248e4cd4a0a6220bc54ac4e702
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2023-10-13 19:01:29 +0200

    Remove old config

--- siguran-sam-expo/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: ["@react-native-community", "plugin:prettier/recommended"],
-  plugins: ["@typescript-eslint", "prettier"],
-  rules: {
-    // Add any custom rules or overrides here
-  },
-};
