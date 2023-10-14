00d60172a7c3aad7feed0ce71b8f0bb057f86ab8
Anastas Radosavljevic <anastasrado@icloud.com>
2023-10-14 15:41:19 +0200

    Added checks for nativeSMSProvider also and included env variables too

--- mobile-app/src/services/sms/providers/NativeSMSService.ts
+++ mobile-app/src/services/sms/providers/NativeSMSService.ts
@@ -1,7 +1,25 @@
 class NativeSMSProvider {
+  private twilioFunctionUrl: string;
+  private recipientLimit: number;
+
+  constructor() { 
+    this.recipientLimit = parseInt(process.env.REACT_APP_RECIPIENT_LIMIT ?? '10', 10);
+    this.twilioFunctionUrl = process.env.REACT_APP_TWILIO_SERVERLESS_FUNCTION_URL ?? '';
+  }
+
   sendSMS(to: string[], body: string): void {
-    if (to.length === 0) {
-      throw new Error('No recipients specified');
+    if(!to || to.length === 0) {
+      throw new Error('Recipient list is empty');
+    }
+
+    if(!body || body.trim().length === 0) {
+      console.log('Body is empty, using default body');
+      body = "Placeholder default body";
+    }
+
+    if (to.length > this.recipientLimit) {
+      console.log(`Recipient limit of ${this.recipientLimit} exceeded, taking first ${this.recipientLimit} numbers.`);
+      to = to.splice(this.recipientLimit);
     }
     
     //Implement NativeSMS functionality here
