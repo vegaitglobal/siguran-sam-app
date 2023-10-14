bf850381a047c6f2be3d6c73eb40bc944098b5a8
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2023-10-14 16:00:56 +0200

    Add icons

--- mobile-app/src/shared/components/icon/icon.data.ts
+++ mobile-app/src/shared/components/icon/icon.data.ts
@@ -5,7 +5,8 @@ import Hamburger from '@/shared/assets/icons/hamburger.svg';
 import ArrowBack from '@/shared/assets/icons/arrow-back.svg';
 import TrashCan from '@/shared/assets/icons/trash-can.svg';
 import AddUser from '@/shared/assets/icons/add-user.svg';
-
+import ArrowBigLeft from '@/shared/assets/icons/arrow-big-left.svg';
+import ArrowBigRight from '@/shared/assets/icons/arrow-big-right.svg';
 export const iconComponents = {
 	home: Home,
 	'star-outline': StarOutline,
@@ -14,6 +15,8 @@ export const iconComponents = {
 	'arrow-back': ArrowBack,
 	'trash-can': TrashCan,
 	'add-user': AddUser,
+	'arrow-big-left': ArrowBigLeft,
+	'arrow-big-right': ArrowBigRight,
 };
 
 export type IconName = keyof typeof iconComponents;
