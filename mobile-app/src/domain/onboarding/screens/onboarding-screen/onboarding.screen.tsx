a6718763fc629fe2237adeebdd770f99b74cc8fb
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2023-10-14 16:29:18 +0200

    Add step handle

--- mobile-app/src/domain/onboarding/screens/onboarding-screen/onboarding.screen.tsx
+++ mobile-app/src/domain/onboarding/screens/onboarding-screen/onboarding.screen.tsx
@@ -6,18 +6,43 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import NextButton from '../../components/next-button';
 import BackButton from '../../components/back-button';
+import { useCallback, useState } from 'react';
+import Label from '@/shared/components/label';
+
+const MAX_STEP = 8;
 
 export interface Props
 	extends NativeStackScreenProps<RootStackParamList, AppScreen.ONBOARDING> {}
 
 const OnboardingScreen = () => {
+	const [step, setStep] = useState(1);
+
+	const nextStep = useCallback(() => {
+		console.log('here');
+		setStep((previous) => {
+			if (previous >= MAX_STEP) return previous;
+
+			return previous + 1;
+		});
+	}, []);
+
+	const previousStep = useCallback(() => {
+		setStep((previous) => {
+			if (previous <= 1) return previous;
+
+			return previous - 1;
+		});
+	}, []);
+
 	return (
 		<ScreenTemplate>
 			<StatusBar style='light' />
-			<View style={styles.mainContentContainer}></View>
+			<View style={styles.mainContentContainer}>
+				<Label>{step}</Label>
+			</View>
 			<View style={styles.buttonsContainer}>
-				<BackButton />
-				<NextButton />
+				<BackButton onPress={previousStep} />
+				<NextButton onPress={nextStep} />
 			</View>
 		</ScreenTemplate>
 	);
