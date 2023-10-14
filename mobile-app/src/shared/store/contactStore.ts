9cd77b4ea2dbd1fb4f47c04c7314fbcd5b8612e9
mrmi <nikola.31023@gmail.com>
2023-10-14 20:35:53 +0200

    Implemented custom exceptions in contact store, functions now return all contacts

--- mobile-app/src/shared/store/contactStore.ts
+++ mobile-app/src/shared/store/contactStore.ts
@@ -2,13 +2,28 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-type Contact = {
+export type Contact = {
 	name: string;
 	phoneNumber: string;
+	id: string;
 };
 interface ContactStoreState {
 	contacts: Contact[];
 }
+class DuplicateContactError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = 'Duplicate contact';
+	}
+}
+class ContactsFullError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = 'Emergency contacts full';
+	}
+}
+
+const maxContacts = 3;
 
 export const contactStore = create<ContactStoreState>()(
 	persist(
@@ -22,11 +37,16 @@ export const contactStore = create<ContactStoreState>()(
 	)
 );
 
-export const getContacts = () => contactStore.getState();
+export const getContacts = () => contactStore.getState().contacts;
 
-export const addContact = (contact: Contact) => {
+export const addContact = (contact: Contact): Contact[] => {
+	if (getContacts().length == maxContacts) {
+		throw new ContactsFullError(
+			'Emergency contacts cannot exceed max capacity.'
+		);
+	}
 	if (contactStore.getState().contacts.includes(contact)) {
-		throw new Error(
+		throw new DuplicateContactError(
 			'Contact with number: ' +
 				contact.phoneNumber +
 				' and name ' +
@@ -38,14 +58,20 @@ export const addContact = (contact: Contact) => {
 	contactStore.setState((state) => ({
 		contacts: [...state.contacts, contact],
 	}));
+
+	return getContacts();
 };
 
-export const deleteContact = (contact: Contact) =>
+export const deleteContact = (contact: Contact): Contact[] => {
 	contactStore.setState((state) => ({
 		contacts: state.contacts.filter(
 			(c) => contact.phoneNumber !== c.phoneNumber
 		),
 	}));
+	return getContacts();
+};
 
-export const deleteAllContacts = () =>
+export const deleteAllContacts = (): Contact[] => {
 	contactStore.setState(() => ({ contacts: [] }));
+	return getContacts();
+};
