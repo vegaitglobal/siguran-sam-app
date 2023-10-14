30355b7915755b8b1b3bd72e0dfdee19dbee5ef7
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2023-10-14 20:28:26 +0200

    Add tutorial

--- mobile-app/src/domain/onboarding/components/main-onboarding-component/main-onboarding-component.tsx
+++ mobile-app/src/domain/onboarding/components/main-onboarding-component/main-onboarding-component.tsx
@@ -1,10 +1,12 @@
 import { memo } from 'react';
+import Tutorial from '../tutorial';
 
 interface Props {
 	step: number;
 }
 
 const MainOnboardingComponent = ({ step }: Props) => {
+	if (step <= 6) return <Tutorial step={step} />;
 	return null;
 };
 
