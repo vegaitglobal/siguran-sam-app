e7e39a1348f711f0bf94a0b51611327b3a9224af
Anastas Radosavljevic <anastasrado@icloud.com>
2023-10-14 15:45:08 +0200

    Small changes to NativeSMSService boilerplate file

--- mobile-app/src/services/sms/providers/NativeSMSService.ts
+++ mobile-app/src/services/sms/providers/NativeSMSService.ts
@@ -1,4 +1,4 @@
-class NativeSMSProvider {
+class NativeSMSProvider implements ISMSProvider<void>{
   private twilioFunctionUrl: string;
   private recipientLimit: number;
 
@@ -7,7 +7,7 @@ class NativeSMSProvider {
     this.twilioFunctionUrl = process.env.REACT_APP_TWILIO_SERVERLESS_FUNCTION_URL ?? '';
   }
 
-  sendSMS(to: string[], body: string): void {
+  sendSMS(to: string[], body: string): Promise<void> {
     if(!to || to.length === 0) {
       throw new Error('Recipient list is empty');
     }
@@ -19,10 +19,11 @@ class NativeSMSProvider {
 
     if (to.length > this.recipientLimit) {
       console.log(`Recipient limit of ${this.recipientLimit} exceeded, taking first ${this.recipientLimit} numbers.`);
-      to = to.splice(this.recipientLimit);
+      to = to.splice(0, this.recipientLimit);
     }
     
     //Implement NativeSMS functionality here
+    return Promise.resolve();
   }
 }
 
