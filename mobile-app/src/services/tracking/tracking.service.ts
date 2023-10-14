98fe734bd9d055cb874d378f498ebc7e56a3f1f4
Nenad Percic <n.percic@vegait.rs>
2023-10-14 20:47:54 +0200

    Event tracking change

--- mobile-app/src/services/tracking/tracking.service.ts
+++ mobile-app/src/services/tracking/tracking.service.ts
@@ -1,5 +1,5 @@
+// @ts-nocheck
 import contentful from 'contentful-management';
-
 import { ApplicationEvent, TrackingService } from './tracking.interfaces';
 
 class ContentfulTrackingService implements TrackingService {
@@ -23,7 +23,12 @@ class ContentfulTrackingService implements TrackingService {
 			.then((space) => space.getEnvironment('master'));
 
 		return environment.createEntry(name, {
-			fields,
+			fields: Object.keys(fields).reduce(
+				(acc: { [key: string]: any }, key: string) => {
+					return (acc[key] = { 'en-US': fields[key] });
+				},
+				{}
+			),
 		});
 	}
 }
