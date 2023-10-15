d9d4504cab20f68293dc30e0ca047117bbfc9888
v.jankovic <v.jankovic@vegaitsourcing.rs>
2023-10-15 16:00:50 +0200

    Removed dotenv package

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,9 +1,7 @@
-require("dotenv").config();
-
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [['module:react-native-dotenv', { moduleName: '@env' }],'react-native-reanimated/plugin']
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: ['react-native-reanimated/plugin'],
+	};
 };
