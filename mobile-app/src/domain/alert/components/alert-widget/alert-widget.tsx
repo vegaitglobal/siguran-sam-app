04d58d899d132e6f0c990febe5b3c07c5f4e60f7
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2024-03-03 01:44:02 +0100

    Refactor alert widget

---
+++ mobile-app/src/domain/alert/components/alert-widget/alert-widget.tsx
@@ -0,0 +1,76 @@
+import Label from '@/shared/components/label';
+import { DeviceLocation } from '@/shared/types';
+import React, { Fragment, useCallback, useEffect, useMemo, useState } from 'react';
+import { ActivityIndicator, Text } from 'react-native';
+import CircleButton from '../circle-button';
+import Moment from 'react-moment';
+import { sendEmergencyRequest } from '../../services/sms-service';
+import { useUserInfoStore } from '@/shared/store';
+import { useContactStore } from '@/shared/store/use-contact-store';
+
+type Props = {
+  location: DeviceLocation | undefined;
+};
+
+const AlertWidget = ({ location }: Props) => {
+  const { fullName } = useUserInfoStore();
+  const { contacts } = useContactStore();
+
+  const [minutes, setMinutes] = useState(0);
+  const [hint, setHint] = useState<string>();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMinutes((old) => old - 1);
+    }, 60_000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const onCancel = useCallback(() => {
+    if (minutes <= 0) setHint('Držite dugme 2 sekunde');
+  }, [minutes]);
+
+  const onComplete = useCallback(() => {
+    sendEmergencyRequest(contacts, location, fullName);
+    setMinutes(5);
+    setHint('Sigurnosni kontakti su obavešteni');
+  }, [contacts, fullName, location]);
+
+  const disabled = useMemo(() => minutes > 0, [minutes]);
+
+  if (!location)
+    return (
+      <Fragment>
+        <Label style={{ marginBottom: 16, textAlign: 'center' }}>
+          Pronalaženje Vaše lokacije...
+        </Label>
+        <ActivityIndicator size='large' color={'#fff'} />
+      </Fragment>
+    );
+
+  return (
+    <Fragment>
+      <CircleButton
+        hint={hint}
+        onCancel={onCancel}
+        onComplete={onComplete}
+        disabled={disabled}
+        minutes={5}
+        delay={2000}
+      />
+      <Label style={{ marginBottom: 12, fontSize: 20, fontWeight: 'bold' }}>
+        {location.city}, {location.country}
+      </Label>
+      <Label type='pItalic'>Poslednja zabeležena lokacija</Label>
+      <Label type='pItalic'>
+        je od{' '}
+        <Moment element={Text} locale='sr' fromNow>
+          {location.timestamp}
+        </Moment>
+      </Label>
+      {location.accuracy && <Label type='pItalic'>sa preciznošću od {location.accuracy}</Label>}
+    </Fragment>
+  );
+};
+
+export default AlertWidget;
--- mobile-app/src/domain/alert/screens/alert.screen.tsx
+++ mobile-app/src/domain/alert/screens/alert.screen.tsx
@@ -15,6 +15,7 @@ import { useUserInfoStore } from '@/shared/store';
 import { useContactStore } from '@/shared/store/use-contact-store';
 import { sendEmergencyRequest } from '../services/sms-service';
 import LocationPermissionDenied from '../components/location-permission-denied';
+import AlertWidget from '../components/alert-widget';
 
 export interface Props
   extends CompositeScreenProps<
@@ -23,94 +24,11 @@ export interface Props
   > {}
 
 const AlertScreen = () => {
-  const { fullName } = useUserInfoStore();
-  const { contacts } = useContactStore();
-
-  const [minutes, setMinutes] = useState(0);
-  const [hint, setHint] = useState<string>();
-
   const { isPermissionGranted, location } = useLocation();
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setMinutes((old) => old - 1);
-    }, 60_000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const onCancel = useCallback(async () => {
-    if (minutes <= 0) setHint('Držite dugme 2 sekunde');
-  }, [minutes]);
-
-  const onComplete = useCallback(async () => {
-    sendEmergencyRequest(contacts, location, fullName);
-    setMinutes(5);
-    setHint('Sigurnosni kontakti su obavešteni');
-  }, [contacts, fullName, location]);
-
-  const disabled = useMemo(() => minutes > 0, [minutes]);
-
-  const { locationTimestamp, accuracy, city, country } = useMemo(() => {
-    if (location === undefined) {
-      return {};
-    }
-    const { accuracy, timestamp, city, country } = location;
-    return {
-      accuracy,
-      locationTimestamp: timestamp,
-      city,
-      country,
-    };
-  }, [location]);
-
-  const LocationPermissionGrantedScreen = useCallback(() => {
-    return (
-      <Fragment>
-        <CircleButton
-          hint={hint}
-          onCancel={onCancel}
-          onComplete={onComplete}
-          disabled={disabled}
-          minutes={5}
-          delay={2000}
-        />
-        <Label style={{ marginBottom: 12, fontSize: 20, fontWeight: 'bold' }}>
-          {city}, {country}
-        </Label>
-        <Label type='pItalic'>Poslednja zabeležena lokacija</Label>
-        <Label type='pItalic'>
-          je od{' '}
-          <Moment element={Text} locale='sr' fromNow>
-            {locationTimestamp}
-          </Moment>
-        </Label>
-        {accuracy && <Label type='pItalic'>sa preciznošću od {accuracy}</Label>}
-      </Fragment>
-    );
-  }, [accuracy, city, country, locationTimestamp, disabled, hint, onComplete, onCancel]);
-
-  const LocationLoadingScreen = useCallback(() => {
-    return (
-      <Fragment>
-        <Label style={{ marginBottom: 16, textAlign: 'center' }}>
-          Pronalaženje Vaše lokacije...
-        </Label>
-        <ActivityIndicator size='large' color={'#fff'} />
-      </Fragment>
-    );
-  }, []);
 
   return (
     <View style={styles.container}>
-      {isPermissionGranted ? (
-        location === undefined ? (
-          <LocationLoadingScreen />
-        ) : (
-          <LocationPermissionGrantedScreen />
-        )
-      ) : (
-        <LocationPermissionDenied />
-      )}
+      {isPermissionGranted ? <AlertWidget location={location} /> : <LocationPermissionDenied />}
     </View>
   );
 };
