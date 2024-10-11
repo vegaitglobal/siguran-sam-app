7e6705a60d10e2d8ef9e89089b97c0407891e7ba
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2024-10-11 22:02:35 +0200

    Increase max contacts to 5

--- mobile-app/src/shared/store/use-contact-store.ts
+++ mobile-app/src/shared/store/use-contact-store.ts
@@ -4,81 +4,73 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 import { Contact } from '../types';
 
 interface ContactStoreState {
-	contacts: Contact[];
+  contacts: Contact[];
 }
 export class DuplicateContactError extends Error {
-	constructor(message: string) {
-		super(message);
-		this.name = 'Duplicate contact';
-	}
+  constructor(message: string) {
+    super(message);
+    this.name = 'Duplicate contact';
+  }
 }
 
 export class LastContactDeletionError extends Error {
-	constructor(message: string) {
-		super(message);
-		this.name = 'Must have at least one contact';
-	}
+  constructor(message: string) {
+    super(message);
+    this.name = 'Must have at least one contact';
+  }
 }
 
 export class ContactsFullError extends Error {
-	constructor(message: string) {
-		super(message);
-		this.name = 'Emergency contacts full';
-	}
+  constructor(message: string) {
+    super(message);
+    this.name = 'Emergency contacts full';
+  }
 }
 
-const maxContacts = 3;
+const maxContacts = 5;
 
 export const useContactStore = create<ContactStoreState>()(
-	persist(
-		(_set) => ({
-			contacts: [],
-		}),
-		{
-			name: 'contact-storage',
-			storage: createJSONStorage(() => AsyncStorage),
-		}
-	)
+  persist(
+    (_set) => ({
+      contacts: [],
+    }),
+    {
+      name: 'contact-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
 );
 
 export const getContacts = () => useContactStore.getState().contacts;
 
 export const addContact = (contact: Contact): Contact[] => {
-	if (getContacts().length == maxContacts) {
-		throw new ContactsFullError(
-			'Emergency contacts cannot exceed max capacity.'
-		);
-	}
-	if (
-		useContactStore.getState().contacts.find((c) => c.number === contact.number)
-	) {
-		throw new DuplicateContactError(
-			'Contact with number: ' +
-				contact.number +
-				' and name ' +
-				contact.name +
-				' already exists.'
-		);
-	}
+  if (getContacts().length == maxContacts) {
+    throw new ContactsFullError('Emergency contacts cannot exceed max capacity.');
+  }
+  if (useContactStore.getState().contacts.find((c) => c.number === contact.number)) {
+    throw new DuplicateContactError(
+      'Contact with number: ' + contact.number + ' and name ' + contact.name + ' already exists.'
+    );
+  }
 
-	useContactStore.setState((state) => ({
-		contacts: [...state.contacts, contact],
-	}));
+  useContactStore.setState((state) => ({
+    contacts: [...state.contacts, contact],
+  }));
 
-	return getContacts();
+  return getContacts();
 };
 
 export const deleteContact = (number: string): Contact[] => {
-	if (getContacts().length === 1) {
-		throw new LastContactDeletionError('Must have at least one contact');
-	}
-	useContactStore.setState((state) => ({
-		contacts: state.contacts.filter((c) => number !== c.number),
-	}));
-	return getContacts();
+  if (getContacts().length === 1) {
+    throw new LastContactDeletionError('Must have at least one contact');
+  }
+  useContactStore.setState((state) => ({
+    contacts: state.contacts.filter((c) => number !== c.number),
+  }));
+  return getContacts();
 };
 
 export const deleteAllContacts = (): Contact[] => {
-	useContactStore.setState(() => ({ contacts: [] }));
-	return getContacts();
+  useContactStore.setState(() => ({ contacts: [] }));
+  return getContacts();
 };
